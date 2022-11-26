d00cf7853ac934123d57173d079128d68172b0b9
Olga <olchik-n@mail.ru>
2022-11-26 23:28:41 +0300

    задача 13

---
+++ task10/Program.cs
@@ -0,0 +1,21 @@
+﻿//int number=Console.WriteLine (100, 1000);
+/*Console.WriteLine ("ввести трехзначное число");
+int a =Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine($"Вторая цифра трехзначного числа ='{a}'");
+
+
+
+/*Console.WriteLine(number);
+
+if(number > 99)
+{
+    Console.WriteLine((number / 100) % 10);
+}
+*/
+Console.WriteLine("Введите трехзначное число");
+
+string input=Console.ReadLine();
+if(!input.Equals("exit"))
+Console.WriteLine("вторая цифра {1}",input, input[1]);
